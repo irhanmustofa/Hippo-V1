ac747df1a88cee6a35999d856fb219fa3352fc90
HenriHerdiyanto <henri.herdiyanto@students.amikom.ac.id>
2023-02-23 16:19:53 +0700

    update-icon

--- header.php
+++ header.php
@@ -22,7 +22,7 @@ if (!isset($_SESSION['start'])) {
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
     <!-- Favicon -->
-    <link rel="icon" href="asset/img/core-img/favicon.ico">
+    <link rel="icon" href="asset/img/bg-img/logo-kudanil.png">
     <link rel="apple-touch-icon" href="asset/img/icons/icon-96x96.png">
     <link rel="apple-touch-icon" sizes="152x152" href="asset/img/icons/icon-152x152.png">
     <link rel="apple-touch-icon" sizes="167x167" href="asset/img/icons/icon-167x167.png">
@@ -57,9 +57,9 @@ if (!isset($_SESSION['start'])) {
             <!-- # Copy the code from here ... -->
             <!-- Header Content -->
             <div class="header-content header-style-five position-relative d-flex align-items-center justify-content-between">
-                <div class="logo-wrapper"><a href="page-home.html"><img src="asset/img/profile/logo.jpg" alt=""></a></div>
+                <div class="logo-wrapper"><a href="index.php"><img src="asset/img/core-img/logo-hippo.png" alt=""></a></div>
                 <!-- Logo Wrapper -->
-                <div class="logo-wrapper"><a href="page-home.html"><img src="img/core-img/logo-hippo.png" alt=""></a></div>
+                <!-- <div class="logo-wrapper"><a href="page-home.html"><img src="img/core-img/logo-hippo.png" alt=""></a></div> -->
                 <!-- Navbar Toggler -->
                 <div class="navbar--toggler" id="affanNavbarToggler" data-bs-toggle="offcanvas" data-bs-target="#affanOffcanvas" aria-controls="affanOffcanvas"><span class="d-block"></span><span class="d-block"></span><span class="d-block"></span></div>
             </div>
